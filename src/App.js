96d1810a6159c4f0ee5025a3f27c0e3c62a64e2b
Kévin Rocha <neonix75@gmail.com>
2020-09-01 15:00:08 +0200

    Create basics componants

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import PathfindingVisualizer from './componantes/PathfindingVisualizer.jsx';
+
 function App() {
   return (
     <div className="App">
       <h1>Pathfinding</h1>
+      <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
 }
---
+++ src/componantes/PathfindingVisualizer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/PathfindingVisualizer.css'
+
+
+
+function PathfindingVisualizer() {
+
+
+    return (
+        <div className="PathfindingVisualizer">
+
+        </div>
+    )
+}
+
+export default PathfindingVisualizer;
