b011087b1a5aa8ce559f636dd67fe1612f75f613
Kévin Rocha <neonix75@gmail.com>
2020-09-02 18:49:49 +0200

    Getting all the nodes

--- src/algorithm/dijkstra.js
+++ src/algorithm/dijkstra.js
@@ -2,9 +2,23 @@
 
 
 function dijkstra(grid, startNode, finishNode) {
-    
+    const visitedNodesInOrder = [];
+    startNode.distance = 0;
+    const unvisitedNodes = getAllNodes(grid);
+    console.log(unvisitedNodes);
 }
 
+
+
+function getAllNodes(grid) {
+    const nodes = [];
+    for (const row of grid) {
+        for (const node of row) {
+            nodes.push(node);
+        }
+    }
+    return nodes;
+}
 
 
 
