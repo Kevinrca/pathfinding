16431e075fe9e9bf4a3241d4a6f8233a106b6af7
Kévin Rocha <neonix75@gmail.com>
2020-09-03 13:25:40 +0200

    get the neighbors of the nodes

--- src/algorithm/dijkstra.js
+++ src/algorithm/dijkstra.js
@@ -45,11 +45,19 @@ function sortNodes(unvisitedNodes) {
 }
 
 function updateUnvisitedNeighbors(node, grid) {
-
+    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+    console.log(unvisitedNeighbors);
 }
 
 function getUnvisitedNeighbors(node, grid) {
-    
+    const neighbors = [];
+    const { row, col } = node;
+    if (row > 0) neighbors.push(grid[row - 1][col]);
+    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+    if (col > 0) neighbors.push(grid[row][col - 1]);
+    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+
+    return neighbors.filter(neighbor => !neighbor.isVisited);
 }
 
 
