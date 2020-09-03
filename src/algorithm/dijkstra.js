1fc3437e711d4026c5e1a6b126819f35e4611634
Kévin Rocha <neonix75@gmail.com>
2020-09-03 11:23:29 +0200

    writing the dijkstra algorithm

--- src/algorithm/dijkstra.js
+++ src/algorithm/dijkstra.js
@@ -1,12 +1,33 @@
 
 
 
+
+
 function dijkstra(grid, startNode, finishNode) {
+    if(!startNode || !finishNode || startNode === finishNode) {
+        return false;
+    }
+
     const visitedNodesInOrder = [];
     startNode.distance = 0;
     const unvisitedNodes = getAllNodes(grid);
-    console.log(unvisitedNodes);
+
+    while(!!unvisitedNodes.length) {
+        sortNodes(unvisitedNodes);
+        const closestNode = unvisitedNodes.shift();
+        closestNode.isVisited = true;
+        visitedNodesInOrder.push(closestNode);
+
+        console.log(closestNode);
+
+        if(closestNode === finishNode) {
+            console.log('success');
+            return visitedNodesInOrder;
+        }
+    }
 }
+
+
 
 
 
@@ -18,6 +39,10 @@ function getAllNodes(grid) {
         }
     }
     return nodes;
+}
+
+function sortNodes(unvisitedNodes) {
+    unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance );
 }
 
 
